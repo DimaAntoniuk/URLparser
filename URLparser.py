268735910869d16e4ce7c99bada17b00e49b73e2
Dima Antoniuk <dimaantoniuk09@gmail.com>
2019-07-08 17:09:15 +0300

    v2.2 clean code

--- URLparser.py
+++ URLparser.py
@@ -22,34 +22,37 @@ for url in urls:
             for line in tuple:
                 output = open('output.csv', 'a')
                 for key_word in key_words:
-                    if(re.search('>.*' + key_word, line)
+                    if (re.search('>.*' + key_word, line)
                             or re.search('href\s*=\s*".*' + key_word, line)
                             or re.search("href\s*=\s*'.*" + key_word, line)):
                         line = line[line.find('href'):]
+
                         single_quotes=''
-                        if(line.find("'")>=0):
+                        if (line.find("'")>=0):
                             single_quotes = line[line.find("'")+1:]
+
                         double_quotes = ''
-                        if(line.find('"')>=0):
+                        if (line.find('"')>=0):
                             double_quotes = line[line.find('"')+1:]
                         link = ''
-                        if(single_quotes):
+                        if (single_quotes):
                             link = single_quotes[:single_quotes.find("'")]
-                        elif(link==''):
+                        elif (link==''):
                             link = double_quotes[:double_quotes.find('"')]
                         link = link.strip()
 
-                        if(link!='' and link[0] == '/'):
-                            if(domain[len(domain)-1]=='/' and last_link!=domain + link[1:]):
+                        if (link != '' and link[0] == '/'):
+                            if (domain[len(domain)-1] == '/'
+                                    and last_link != domain + link[1:]):
                                 output.write(domain + link[1:] + '\n')
                                 last_link = domain + link[1:]
-                            elif(last_link != domain + link):
+                            elif (last_link != domain + link):
                                 output.write(domain + link + '\n')
                                 last_link = domain + link
-                        elif(link!='' and last_link != link):
+                        elif (link != '' and last_link != link):
                             output.write(link + '\n')
                             last_link = link
                 output.close()
     except:
-        if(url!='http://'):
+        if (url != 'http://'):
             print(url + " is a bad one\n")
