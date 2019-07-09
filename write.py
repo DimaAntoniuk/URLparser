e42801491861101cf4c7a0d831364c88be95043b
Dima Antoniuk <dimaantoniuk09@gmail.com>
2019-07-10 00:18:36 +0300

    v3.2 strong parse

--- write.py
+++ write.py
@@ -1,8 +1,5 @@
 import requests
 import re
 
-link_html = requests.get('https://www.bulkpowders.co.uk/careers-at-bulk-powders').text
-pattern = r'src=[\"\'][^>]+[\"\']'
-link_html = re.sub(pattern, "", link_html)
-if(link_html.find('magento') or link_html.find('Magento')):
-    print("YES")
+if(requests.get('http://www.bugarttia.co.uk/jobs/').text.find('magento')>=0):
+    print('!')
